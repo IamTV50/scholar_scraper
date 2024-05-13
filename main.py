dd98fbeeaefe918b6c60562667e2eb7abe5d4503
Jure_Golob <95366061+IamTV50@users.noreply.github.com>
2024-05-13 12:16:17 +0100

    implemented parsing of google scholar profile

--- main.py
+++ main.py
@@ -1,51 +1,111 @@
 from bs4 import BeautifulSoup
-import requests
 import time
 import json
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver.common.by import By
 
-NAMES_FILE = 'profiles.json'
+#NAMES_FILE = 'profiles.json'
+NAMES_FILE = 'profiles_test.json' # shorter profiles.json
+ARTICLES_FILE = 'atricles.json'
 
-def getSoupPage(url):
-	headers = {
-		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.9999.99 Safari/537.36"
-	}
-	responsePage = requests.get(url, headers=headers)
+def getScholarArticlesLinks(trArticles) -> list:
+	articleLinks = []
+	aClass = 'gsc_a_at'
 
-	soup = BeautifulSoup(responsePage.text, 'html.parser')
-	return soup
+	for el in trArticles:
+		aTag = el.find('a', {'class':aClass})
 
-def parseScholarPage(profilePage):
-	pass
+		articleObject = {}
+		articleObject['title'] = aTag.text
+		articleObject['url'] = 'https://scholar.google.com' + aTag.attrs['href']
+		articleObject['year'] = el.find('span', {'class': 'gsc_a_h gsc_a_hc gs_ibl'}).text
 
-def parseResearchGaetProfile(profilePage):
+		articleLinks.append(articleObject)
+
+	return articleLinks
+
+def parseScholarPage(profilePageUrl) -> list:
+	loadMoreBtnId = 'gsc_bpf_more'
+	articleTrElemClass = 'gsc_a_tr'
+
+	# use headless browser (without gui)
+	options = ChromeOptions()
+	options.add_argument('--headless=new')
+	driver = webdriver.Chrome(options=options)
+
+	# make initial get request
+	driver.get(profilePageUrl)
+
+	# Get session cookies
+	cookies = driver.get_cookies()
+	time.sleep(0.5)
+
+	soup = BeautifulSoup(driver.page_source, 'html.parser')
+	loadMoreBtn = soup.find('button', {'id': loadMoreBtnId})
+
+	while 'disabled' not in loadMoreBtn.attrs:
+		moreBtn = driver.find_element(By.ID, loadMoreBtnId)
+		moreBtn.click()
+
+		time.sleep(2)  # DON'T DELETE!!!
+
+		# Add session cookies to the browser session
+		for cookie in cookies:
+			driver.add_cookie(cookie)
+
+		soup = BeautifulSoup(driver.page_source, 'html.parser')
+		loadMoreBtn = soup.find('button', {"id": loadMoreBtnId})
+		trs = soup.find_all('tr', {"class": articleTrElemClass})
+
+		time.sleep(1)
+
+	# close "browser" to save resources
+	driver.quit()
+
+	if trs == None:
+		return []
+
+	return getScholarArticlesLinks(trs)
+
+
+def parseResearchGaetProfile(profilePageUrl) -> list:
 	pass
 
 def main():
-	with open(NAMES_FILE, 'r', encoding='utf-8') as f:
-		researchers = json.load(f)
-		f.close()
+	try:
+		with open(NAMES_FILE, 'r', encoding='utf-8') as f:
+			researchers = json.load(f)
+			f.close()
+	except FileNotFoundError:
+		print(f'file {NAMES_FILE} not found')
+		exit()
 
+	profilesWithArticles = {}
 	for researcher in researchers:
-		print(researcher)
 		profileUrl = researcher['profileUrl']
 
 		if profileUrl == "":
 			print(f"missing profileUrl for {researcher['fullName']}")
 			continue
 
-		htmlProfile = getSoupPage(profileUrl)
+		profilesWithArticles['fullName'] = researcher['fullName']
+		profilesWithArticles['profileUrl'] = profileUrl
 
 		if profileUrl.startswith('https://scholar.google.com/'):
-			parseScholarPage(htmlProfile)
+			profilesWithArticles['articles'] = parseScholarPage(profileUrl)
 		elif profileUrl.startswith('https://www.researchgate.net/'):
-			parseResearchGaetProfile(htmlProfile)
+			continue
+			#profilesWithArticles['articles'] = parseResearchGaetProfile(profileUrl)
 		else:
 			print(f"unsuported profile {researcher['fullName']} ({profileUrl})")
-			print("profile need to be either 'https://scholar.google.com/...' or 'https://www.researchgate.net/'")
+			print("profile need to be either 'https://scholar.google.com/...' or 'https://www.researchgate.net/...'")
 			print()
 
-
 		time.sleep(5)  # Sleep between each requests to (hopefully) avoid google ip ban...
+
+	with open(ARTICLES_FILE, 'w', encoding='utf-8') as json_file:
+		json.dump(profilesWithArticles, json_file, ensure_ascii=False, indent=4)
 
 def create_jsonNames(input_file, output_file):
 	researchers = []
